8d80aa33c4e58f22b349ea279a3acbfd937d9c7b
Ethan Carty <ethancartybb@gmail.com>
2024-12-16 21:39:41 -0500

    Changing styling. (background black, fixing formatting, etc)

--- Portfolio/src/components/Navbar.tsx
+++ Portfolio/src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../styles/Navbar.css';
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link> |{" "}
-      <Link to="/projects">Projects</Link> |{" "}
-      <Link to="/bio">Bio</Link> |{" "}
-      <Link to="/resume">Resume</Link>
+    <nav className="navbar">
+      <Link to="/" className="nav-link">Home</Link>
+      <Link to="/projects" className="nav-link">Projects</Link>
+      <Link to="/bio" className="nav-link">Bio</Link>
+      <Link to="/resume" className="nav-link">Resume</Link>
     </nav>
   );
 };
