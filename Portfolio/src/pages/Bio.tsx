a4fb41bdf0e5b16c2bf2719c4d9c620e1cd8013e
Ethan Carty <ethancartybb@gmail.com>
2024-12-16 20:08:14 -0500

    Added files for home, project, resume and bio pages

---
+++ Portfolio/src/pages/Bio.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Bio: React.FC = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is my biography. Here's a bit about who I am!</p>
+    </div>
+  );
+};
+
+export default Bio;
---
+++ Portfolio/src/pages/Resume.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Resume: React.FC = () => {
+  return (
+    <div>
+      <h1>My Resume</h1>
+      <p>Here you can find my professional resume.</p>
+    </div>
+  );
+};
+
+export default Resume;
